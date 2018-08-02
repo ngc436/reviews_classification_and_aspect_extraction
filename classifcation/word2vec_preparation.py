c72814b47b816abcc8b41977f1b8fc41cba51841
maria <mariyaxod@yandex.ru>
2018-08-02 15:16:31 +0300

    Update requirements

--- classifcation/word2vec_preparation.py
+++ classifcation/word2vec_preparation.py
@@ -1,10 +1,10 @@
-import gensim
+from gensim.models import Word2Vec
 import codecs
 import multiprocessing
 
-model = gensim.models.Word2Vec.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)
+#model = gensim.models.Word2Vec.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)
 
-IO_DIR = 'data_dir/'
+IO_DIR = 'data_dir'
 
 class Sentences(object):
 
@@ -17,14 +17,21 @@ class Sentences(object):
 
 class w2v_model:
 
-    def __init__(self, domain_name, vec_size=200, window=7,verbose=0):
+    def __init__(self, domain_name, vec_size=200, window=7, min_count=2, verbose=0):
         self.domain = domain_name
         self.vec_size = vec_size
         self.window = window
+        self.min_count = min_count
         self.verbose = verbose
 
     def create_model(self):
-        source = IO_DIR + self.domain
+        source = '%s/%s/train.txt' % (IO_DIR, self.domain_name)
+        target = '%s/%s/w2v_embedding' % (IO_DIR, self.domain_name)
+        workers = multiprocessing.cpu_count()
+        sg = True
+        sentences = Sentences(source, size=self.vec_size, )
+        model = Word2Vec()
+        print("model is saved: $s")
 
 
     def read_data(self):
